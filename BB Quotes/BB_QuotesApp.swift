a495cad5f4abf919200a9c113b17b4f30eaf53af
MohamedGoush <0x1110111@gmail.com>
2023-11-02 07:28:14 +0530

    Initial Commit

---
+++ BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by python on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
