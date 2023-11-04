f56868f85075ebb25ce5e3be9a72b054b93d1b89
MohamedGoush <0x1110111@gmail.com>
2023-11-04 17:45:45 +0530

    images and resources are uploaded

--- BB Quotes/ContentView.swift
+++ BB Quotes/ContentView.swift
@@ -9,16 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+       
+        TabView{
+            
+            Text("Breaking Bad View")
+                .tabItem { Label("Breaking Bad", systemImage: "tortoise") }
+            
+            Text("Better Call Saul View")
+                .tabItem { Label("Better Call Saul", systemImage: "briefcase") }
         }
-        .padding()
+        .onAppear{
+            UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance()
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
 #Preview {
     ContentView()
+        
 }
